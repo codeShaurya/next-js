99af6e907cf04fb713491bf6ed03f40a7809b41f
Shubham Maurya <code.shaurya@gmail.com>
2018-10-24 09:53:24 +0530

    all the basic were covered

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 export default () => (
   <div>
+    <Link href="/">
+      <button>> Go the Home page Button </button>
+    </Link>
+    <Link href="/">
+      <a style={{ fontSize: "30px" }}>Go to Home Page Link</a>
+    </Link>
     <p>This is the about page</p>
+    <a href="/">This is not the client routing</a>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
-    <p>Hello Next.js there</p>
+    <Link href="/about">
+      <button>> Go the about page Button </button>
+    </Link>
+    <Link href="/about">
+      <a style={{ fontSize: "30px" }}>Go to about Page Link</a>
+    </Link>
+    <p>Hello Next.js</p>
+    <a href="/about">This is not the client routing</a>
   </div>
 );
 
